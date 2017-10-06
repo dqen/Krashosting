ea02f3225bc61124f62ed459e2b490885c1e724f
dqen <diegobosch17@gmail.com>
2017-10-06 11:25:05 +0200

    index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+    <link rel="stylesheet" href="css/master.css" type="text/css">
+
+</head>
+<body>
+    <div class="banner">
+
+    </div>
+    <ul class="menu">
+        <li class="menuitem"><a href="/index.html">Home</a></li>
+        <li class="menuitem"><a href="pages/contact.html">Contact</a></li>
+    </ul>
+    <div class="container">
+        <div id="pakket1"></div>
+        <div id="pakket2"></div>
+        <div id="pakket3"></div>
+        <br>
+
+</body>
+</html>

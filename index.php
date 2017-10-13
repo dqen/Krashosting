cc86189fd4583220179f446b9c1b1b986bc883be
robinmeijs <robinmeijs@hotmail.com>
2017-10-13 10:04:40 +0200

    front end

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <div class="banner">
     </div>
     <ul class="menu">
-        <li class="menuitem"><a href="/index.html">Home</a></li>
+        <li class="menuitem"><a href="/index.php">Home</a></li>
         <li class="menuitem"><a href="pages/contact.html">Contact</a></li>
         <li class="menuitem"><a href="pages/krashosting.php">login</a></li>
     </ul>
@@ -24,6 +24,10 @@
         <div id="pakket3"></div>
         <div id="pakket4"></div>
         <br>
+
+        <div id="nieuws1"></div>
+        <div id="nieuws2"></div>
+        <div id="nieuws3"></div>
     </div>
 </body>
 </html>

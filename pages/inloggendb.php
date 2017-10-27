9f31d182ceeac8c0945dd04640ade1e4daaece5f
robinmeijs <robinmeijs@hotmail.com>
2017-10-27 09:58:44 +0200

    front end

--- pages/inloggendb.php
+++ pages/inloggendb.php
@@ -1,36 +1,31 @@
 <?php 
-session_start();
-$email = $_POST["email"];
-$mysqli = new mysqli("localhost", "root", "root", "login_krashosting");
-$sql3 = "SELECT attempt FROM login WHERE email = '$email';";
-$query = $mysqli->query($sql3);
-$r= $query->fetch_assoc();
+var_dump($_COOKIE);
+var_dump($_POST);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    $mysqli = new mysqli("localhost", "root", "root", "login_krashosting");
-    $sql = "SELECT wachtwoord, email, admin, attempt FROM login WHERE email = '$email';";
-    $query = $mysqli->query($sql);
-    $r= $query->fetch_assoc();
-    if($r["attempt"] >= 2){
-    $_SESSION["error"] = "<div id='errormsg'><p id='errormsg'> je account is voor veiligheidsredenen geblokkeerd.<br> contacteer de admin voor verderen informatie</p></div>";
-    header("location:krashosting.php");
-    }
-    if ($r["email"] === $_POST["email"] && $r["wachtwoord"] === $_POST["wachtwoord"]){
-        $sql2 = "UPDATE login SET attempt = 0 WHERE email = '$email';";
-        $query = $mysqli->query($sql2);
-        header("location:../index.php");
+    if($_COOKIE["error"] > 3){
+        setcookie("text" ,  "window.confirm('tevaak ingvuld');",0);
+        // header("location:krashosting.php");
         exit();
     }else{
-        $attempts = $r["attempt"];
-        $attempts++;
-        $_SESSION["fout"] = "wachtwoord of gebruikersnaam is fout";
-        $mysqli1 = new mysqli("localhost", "root", "root", "login_krashosting");
-        $sql1 = "UPDATE login SET attempt = $attempts WHERE email = '$email';";
-        $query = $mysqli1->query($sql1);
-        header("location:krashosting.php");
-        exit();
+        $email = $_POST["email"];
+        $mysqli = new mysqli("localhost", "root", "root", "login_krashosting");
+        $sql = "SELECT wachtwoord, email, admin FROM login WHERE email = '$email';";
+        $query = $mysqli->query($sql);
+        $r= $query->fetch_assoc();
+        if ($r["email"] === $_POST["email"] && $r["wachtwoord"] === $_POST["wachtwoord"]){
+            // header("location:../index.php");
+            exit();
+        }else{
+            $value = $_COOKIE["error"]++;
+            // $value = $value++;
+            setcookie("error", $_COOKIE["error"], 0);
+            setcookie("text" , "wachtwoord of gebruikersnaam fout", 0);
+            // header("location:krashosting.php");
+            exit();
+        }
     }
 }else{
-header("location:krashosting.php");
+    // header("location:krashosting.php");
+    exit();
 }
 ?>

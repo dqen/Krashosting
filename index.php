89875379ee1d9cd8b9b25d4bb2f8a73449a0084d
geertsoetens <geert.soetens@ictmbo.nl>
2017-10-27 10:13:11 +0200

    update_nieuwsitem now works!

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require_once('databases/newsdatabase/connect_newsdatabase.php');
+
+    $article = new News();
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +28,7 @@
         <li class="menuitem"><a href="pages/pakketen.html">paketten</a></li>
     </ul>
     <div class="container">
-            <div id="pakket1"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et tortor in sem condimentum convallis nec congue ante. Donec orci velit, faucibus quis venenatis ut, pretium ac sapien. Phasellus eu turpis nunc. Phasellus dignissim tortor sit amet lorem gravida, eget tristique turpis condimentum. In hac habitasse platea dictumst. Morbi pharetra lectus et pellentesque imperdiet. Phasellus velit nibh, dictum non nisi et, rhoncus semper lectus. Nulla ultrices mollis enim at scelerisque. Nam consequat blandit eros ac lobortis. Sed vestibulum diam eget mauris finibus dignissim. Integer ex ligula, tempor ac ornare quis, pretium ut purus.</p></div>
+            <div id="pakket1"><h3><?php echo $article->get_item(1, "header"); ?></h3> <p><?php echo $article->get_item(1, "articlesnippet"); ?></p></div>
             <div id="pakket2"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et tortor in sem condimentum convallis nec congue ante. Donec orci velit, faucibus quis venenatis ut, pretium ac sapien. Phasellus eu turpis nunc. Phasellus dignissim tortor sit amet lorem gravida, eget tristique turpis condimentum. In hac habitasse platea dictumst. Morbi pharetra lectus et pellentesque imperdiet. Phasellus velit nibh, dictum non nisi et, rhoncus semper lectus. Nulla ultrices mollis enim at scelerisque. Nam consequat blandit eros ac lobortis. Sed vestibulum diam eget mauris finibus dignissim. Integer ex ligula, tempor ac ornare quis, pretium ut purus.</p></div>
             <div id="pakket3"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et tortor in sem condimentum convallis nec congue ante. Donec orci velit, faucibus quis venenatis ut, pretium ac sapien. Phasellus eu turpis nunc. Phasellus dignissim tortor sit amet lorem gravida, eget tristique turpis condimentum. In hac habitasse platea dictumst. Morbi pharetra lectus et pellentesque imperdiet. Phasellus velit nibh, dictum non nisi et, rhoncus semper lectus. Nulla ultrices mollis enim at scelerisque. Nam consequat blandit eros ac lobortis. Sed vestibulum diam eget mauris finibus dignissim. Integer ex ligula, tempor ac ornare quis, pretium ut purus.</p></div>
             <div id="pakket4"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et tortor in sem condimentum convallis nec congue ante. Donec orci velit, faucibus quis venenatis ut, pretium ac sapien. Phasellus eu turpis nunc. Phasellus dignissim tortor sit amet lorem gravida, eget tristique turpis condimentum. In hac habitasse platea dictumst. Morbi pharetra lectus et pellentesque imperdiet. Phasellus velit nibh, dictum non nisi et, rhoncus semper lectus. Nulla ultrices mollis enim at scelerisque. Nam consequat blandit eros ac lobortis. Sed vestibulum diam eget mauris finibus dignissim. Integer ex ligula, tempor ac ornare quis, pretium ut purus.</p></div>

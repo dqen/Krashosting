474b2f7ea507c8b8e95f491b73d57ce15b471a42
Gijs <gijs.verhoeven@ictmbo.nl>
2017-12-01 10:57:45 +0100

    weetje

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
     <meta charset="UTF-8">
     <title>home</title>
     <link rel="stylesheet" href="css/master.css" type="text/css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 </head>
 <body>
@@ -22,26 +23,87 @@
         <div class="banner">
         </div>
     <ul class="menu">
-        <li class="active"><a href="/index.php">Home</a></li>
+        <li class= "menuitem "id="active"><a href="/index.php">Home</a></li>
         <li class="menuitem"><a href="pages/contact.html">Contact</a></li>
         <li class="menuitem"><a href="pages/over_ons.html">over ons</a></li>
-        <li class="menuitem"><a href="pages/pakketen.php">paketten</a></li>
+        <li class="menuitem"><a href="pages/pakketen.php">paketten</a></li>2
     </ul>
-        <div class="blended_grid">
-            <div class="pageColumnLeft">
-                <h2>Pakket 1</h2>
+<!--        <div class="blended_grid">-->
+<!--            <div class="pageColumnLeft">-->
+<!--                <h2>Pakket 1</h2>-->
+<!--            </div>-->
+<!--            <div class="pageColumnMid">-->
+<!--                <h2>Pakket 2</h2>-->
+<!--            </div>-->
+<!--            <div class="pageColumnRight">-->
+<!--                <h2>Pakket 3</h2>-->
+<!--            </div>-->
+<!--            <div class="pageFooter">-->
+<!--                <h2>Custom Pakket</h2>-->
+<!--            </div>-->
+<!--        </div>-->
+        <div class="w3-row-padding w3-center w3-padding-64" id="pricing">
+            <h2>PRICING</h2>
+            <p>Choose a pricing plan that fits your needs.</p><br>
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-theme">
+                        <p class="w3-xlarge">Basic</p>
+                    </li>
+                    <li class="w3-padding-16"><b>10GB</b> Storage</li>
+                    <li class="w3-padding-16"><b>10</b> Emails</li>
+                    <li class="w3-padding-16"><b>10</b> Domains</li>
+                    <li class="w3-padding-16"><b>Endless</b> Support</li>
+                    <li class="w3-padding-16">
+                        <h2 class="w3-wide"><i class="fa fa-usd"></i> 10</h2>
+                        <span class="w3-opacity">per month</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24">
+                        <button class="w3-button w3-teal w3-padding-large"><i class="fa fa-check"></i> Sign Up</button>
+                    </li>
+                </ul>
             </div>
-            <div class="pageColumnMid">
-                <h2>Pakket 2</h2>
+
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-theme-l2">
+                        <p class="w3-xlarge">Pro</p>
+                    </li>
+                    <li class="w3-padding-16"><b>25GB</b> Storage</li>
+                    <li class="w3-padding-16"><b>25</b> Emails</li>
+                    <li class="w3-padding-16"><b>25</b> Domains</li>
+                    <li class="w3-padding-16"><b>Endless</b> Support</li>
+                    <li class="w3-padding-16">
+                        <h2 class="w3-wide"><i class="fa fa-usd"></i> 25</h2>
+                        <span class="w3-opacity">per month</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24">
+                        <button class="w3-button w3-teal w3-padding-large"><i class="fa fa-check"></i> Sign Up</button>
+                    </li>
+                </ul>
             </div>
-            <div class="pageColumnRight">
-                <h2>Pakket 3</h2>
-            </div>
-            <div class="pageFooter">
-                <h2>Custom Pakket</h2>
+
+            <div class="w3-third w3-margin-bottom">
+                <ul class="w3-ul w3-border w3-hover-shadow">
+                    <li class="w3-theme">
+                        <p class="w3-xlarge">Premium</p>
+                    </li>
+                    <li class="w3-padding-16"><b>50GB</b> Storage</li>
+                    <li class="w3-padding-16"><b>50</b> Emails</li>
+                    <li class="w3-padding-16"><b>50</b> Domains</li>
+                    <li class="w3-padding-16"><b>Endless</b> Support</li>
+                    <li class="w3-padding-16">
+                        <h2 class="w3-wide"><i class="fa fa-usd"></i> 50</h2>
+                        <span class="w3-opacity">per month</span>
+                    </li>
+                    <li class="w3-theme-l5 w3-padding-24">
+                        <button class="w3-button w3-teal w3-padding-large"><i class="fa fa-check"></i> Sign Up</button>
+                    </li>
+                </ul>
             </div>
         </div>
-    <div id="flexcontainer">
+
+        <div id="flexcontainer">
         <div class="newsdiv">
             <div class="imgdiv" id="img1">
                 <!--                <h4>--><?php //echo $articles->get_item(1, "header"); ?><!--</h4>-->
@@ -81,7 +143,7 @@
             </div>
             <a href="#" class="readmore">READ MORE...</a>
         </div>
-
+c
     </div>
 </div>
 </body>

a339c3586b75ec1564147933a632d2e1b48f2d91
robinmeijs <robinmeijs@hotmail.com>
2017-12-08 12:14:04 +0100

    database update

--- js/betalen.js
+++ js/betalen.js
@@ -1,5 +1,9 @@
-document.addEventListener("DOMContentLoaded", mystart);
-
-function mystart(){
-
-}
+function PayPal() {
+        let txt;
+        if (confirm("Press a button!") == true) {
+            txt = "You pressed OK!";
+        } else {
+            txt = "You pressed Cancel!";
+        }
+        document.getElementsByClassName("PayPal").innerHTML = txt;
+    }

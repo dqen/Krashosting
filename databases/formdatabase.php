a1b3a475a077b43f8644eb22e4427afc4f3bd7ae
geertsoetens <geert.soetens@ictmbo.nl>
2018-01-26 10:31:06 +0100

    update

--- databases/formdatabase.php
+++ databases/formdatabase.php
@@ -6,3 +6,24 @@
  * Time: 10:27
  */
 
+// PLEASE ENTER THE FORM PAGE LOCATION HERE.
+// !! THIS IS A PLACEHOLDER! PLEASE CHANGE. !!
+$formpage = "../pages/form.php";
+
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if ($_POST["comporpart"] === 1) {
+        echo "This is the company check";
+    } else if ($_POST["comporpart"] === 0) {
+        echo "This is the private check";
+    } else {
+        echo "Something went wrong, please contact an administrator. <br>";
+        echo "Dumping admin info... <br>";
+        var_dump($_POST);
+        return;
+    }
+} else {
+    echo "ERROR 104; <br>";
+    echo "ACCESS DENIED.<br><br>";
+    echo "<a href=$formpage>< RETURN</a>";
+}
--- pages/form.php
+++ pages/form.php
@@ -13,7 +13,9 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <style>
-        /* Please do NOT copy this to the document */
+        /*  Please do NOT copy this to the document
+            It's only for placeholder style.
+        */
 
         form input, form label {
             display: block;
@@ -38,12 +40,26 @@
 </head>
 <body>
     <script>
+        function open1() {
+            const form1 = document.getElementById("form1");
+            form1.setAttribute("style", "height: 200px;");
 
+            const form2 = document.getElementById("form2");
+            form2.setAttribute("style", "height: 0px; display: none;")
+        }
+
+        function open2() {
+            const form1 = document.getElementById("form1");
+            form1.setAttribute("style", "height: 0px; display: none;");
+
+            const form2 = document.getElementById("form2");
+            form2.setAttribute("style", "height: 200px;");
+        }
     </script>
     <div>
-        <form autocomplete="on" id="form1">
+        <form id="form1" style="height: 200px;" action="../databases/formdatabase.php" method="POST">
 
-            <input name="comporpart" value="0" hidden>
+            <input type="hidden" name="comporpart" id="comporpart" value="0">
             <label for="name">Name</label>
             <input name="name" id="name" required>
             <label for="lastname">Last Name</label>
@@ -52,15 +68,15 @@
             <input name="tel" id="tel">
             <label for="email">E-mail adress</label>
             <input type="email" name="email" id="email">
-            <label for="adress">Adress & Postal/Zip code</label>
-            <input name="adress" id="adress">
+            <label for="address">Address & Postal/Zip code</label>
+            <input name="address" id="adress">
             <input name="postcode" id="postcode">
             <label for="gender">Gender</label>
             <select name="gender" id="gender">
                 <option value="male">Male</option>
                 <option value="female">Female</option>
             </select>
-            <label>Age</label>
+            <label for="age">Age</label>
             <select name="age" id="age">
                 <?php
 
@@ -70,10 +86,17 @@
 
                 ?>
             </select>
+
+            <br><br>
+
+            <button>Send</button><br>
+            <button type="button" onclick="open2()">Company Form</button>
+
         </form>
 
-        <form autocomplete="on" id="form2">
-            <input name="comporpart" value="1" hidden>
+        <form autocomplete="on" id="form2" style="height: 0; display: none;" action="../databases/formdatabase.php" method="POST">
+
+            <input type="hidden" name="comporpart" id="comporpart" value="1">
             <label for="name">Company Name</label>
             <input name="name" id="name" required>
             <label for="contactpers">Contact Person</label>
@@ -81,7 +104,22 @@
             <label for="emailcontactpers">Email Contact Person</label>
             <input name="emailcontactpers" id="emailcontactpers" required>
             <label for="gendercontactperson">Gender</label>
-            <input name="gendercontactperson">
+            <select name="gendercontactperson" id="gendercontactperson">
+                <option>Male</option>
+                <option>Female</option>
+            </select>
+            <label for="paymentinfo">Payment Details</label>
+            <input name="paymentinfo" id="paymentinfo">
+            <label for="kvk">Chamber of Commerce Number</label>
+            <input name="kvk" id="kvk">
+            <label for="address">Payment Address</label>
+            <input name="address" id="address">
+
+            <br><br>
+
+            <button>Send</button><br>
+            <button type="button" onclick="open1()">Customer Form</button>
+
         </form>
     </div>
 </body>
